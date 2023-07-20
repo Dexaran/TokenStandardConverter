2d297bced3b34dd439b66d8b6256f626bcb2b999
Dexaran <dexaran820@gmail.com>
2023-07-20 17:32:59 +0400

    Readability

--- TokenConverter.sol
+++ TokenConverter.sol
@@ -133,7 +133,7 @@ contract TokenStandardConverter is IERC223Recipient
     function convertERC20toERC223(address _ERC20token, uint256 _amount) public returns (bool)
     {
         require(address(erc223Wrappers[_ERC20token]) != address(0), "ERROR: ERC-223 wrapper for this ERC-20 token does not exist yet.");
-        uint256 _callerBalance    = IERC20(_ERC20token).balanceOf(msg.sender); // Safety variable.
+        uint256 _callerBalance    = IERC20(_ERC20token).balanceOf(msg.sender);    // Safety variable.
         uint256 _converterBalance = IERC20(_ERC20token).balanceOf(address(this)); // Safety variable.
 
         IERC20(_ERC20token).transferFrom(msg.sender, address(this), _amount);
@@ -146,7 +146,6 @@ contract TokenStandardConverter is IERC223Recipient
             "ERROR: The transfer have not subtracted tokens from callers balance.");
 
         erc223Wrappers[_ERC20token].mint(msg.sender, _amount);
-
 
         return true;
     }
